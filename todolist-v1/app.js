b7ce1e7fd047751578f412b96574ae0f3abc3f8f
perpyderp <g4m3rall3n@gmail.com>
2023-03-21 20:27:15 -0400

    Completed ToDoList project; Added EJS Coding Challenge

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -8,54 +9,36 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 let items = [];
+let workItems = [];
+let day = date.getDay();
 
 app.get("/", function(req, res) {
 
-    let today = new Date()
-    // var currentDay = today.getDay()
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    let day = today.toLocaleDateString("en-US", options)
-
-
-    // switch(currentDay) {
-    //     case 0:
-    //         day = "Sunday"
-    //         break;
-    //     case 1:
-    //         day = "Monday"
-    //         break;
-    //     case 2:
-    //         day = "Tuesday"
-    //         break;
-    //     case 3:
-    //         day = "Wednesday"
-    //         break;
-    //     case 4:
-    //         day = "Thursday"
-    //         break;
-    //     case 5:
-    //         day = "Friday"
-    //         break;
-    //     case 6:
-    //         day = "Saturday"
-    //         break;
-    //     default:
-    //         console.log("ERROR: Current day is equal to: " + currentDay)
-    // }
-
-    res.render("list", {kindOfDay: day, newItem: items});
+    res.render("list", {listTitle: day, items: workItems});
 
 });
 
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", items: items});
+});
+
 app.post("/", function(req, res) {
-    let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
-})
+
+    if(req.body.value === "Work") {
+        let workItem = req.body.newItem;
+        workItems.push(workItem);
+        res.redirect("/work");
+    }
+    else {
+        let item = req.body.newItem;
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/about", function(req, res) {
+    res.render("about");
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
