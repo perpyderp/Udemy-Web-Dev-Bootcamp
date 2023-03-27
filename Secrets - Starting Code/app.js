7121069ad17e9d8c79c562207547e03cd95a5e4f
perpyderp <g4m3rall3n@gmail.com>
2023-03-27 07:43:52 -0400

    Successfully register users to database

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -1,1 +1,66 @@
 //jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
+// Included because it removes preparatory warnings for Mongoose 7.
+// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
+mongoose.set('strictQuery', false);
+
+// Define the database URL to connect to.
+const userDB = "mongodb://127.0.0.1:27017/userDB";
+
+// Wait for database to connect, logging an error if there is a problem 
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(userDB);
+}
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+app.get("/register", (req, res) => {
+    res.render("register");
+})
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.params.email,
+        password: req.params.password
+    });
+
+    newUser.save().
+    then(result => res.render("secrets")).
+    catch(error => res.json({error: error.message}));
+})
+
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+})
