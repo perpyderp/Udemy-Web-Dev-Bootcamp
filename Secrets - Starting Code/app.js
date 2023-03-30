144f18a63b1777df8f1c871c54dc3c5afb4e1e39
perpyderp <g4m3rall3n@gmail.com>
2023-03-29 22:54:57 -0400

    Add .env file, added Level 2 security

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -1,9 +1,10 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.use(express.static("public"));
@@ -26,16 +27,14 @@ async function main() {
   await mongoose.connect(userDB);
 }
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
-
-
-
-
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -45,18 +44,34 @@ app.get("/login", (req, res) => {
     res.render("login");
 })
 
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}).
+    then(userFound => {
+        if(userFound) {
+            if(userFound.password === password) {
+                res.render("secrets");
+            }
+        }
+    }).
+    catch(error => res.json({error: error.message}));
+});
+
 app.get("/register", (req, res) => {
     res.render("register");
 })
 
 app.post("/register", (req, res) => {
     const newUser = new User({
-        email: req.params.email,
-        password: req.params.password
+        email: req.body.username,
+        password: req.body.password
     });
-
     newUser.save().
-    then(result => res.render("secrets")).
+    then(result => {
+        res.render("secrets");
+    }).
     catch(error => res.json({error: error.message}));
 })
 
