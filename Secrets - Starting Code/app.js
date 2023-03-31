27859bf21456a00da259c957c7a138eba12801bd
perpyderp <g4m3rall3n@gmail.com>
2023-03-31 08:21:14 -0400

    Implemented OAuth2.0 to Secrets Project

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -4,7 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+// const md5 = require("md5");
+// const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.use(express.static("public"));
@@ -12,6 +20,15 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
 // Included because it removes preparatory warnings for Mongoose 7.
@@ -29,51 +46,175 @@ async function main() {
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+        picture: user.picture
+      });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", (req, res) => {
     res.render("home");
 })
 
-app.get("/login", (req, res) => {
+app.get("/auth/google", 
+    passport.authenticate("google", {scope: ["profile"]})
+)
+
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
+
+app.route("/login") 
+
+.get((req, res) => {
     res.render("login");
 })
 
-app.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+.post((req, res) => {
+    
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-    User.findOne({email: username}).
-    then(userFound => {
-        if(userFound) {
-            if(userFound.password === password) {
-                res.render("secrets");
-            }
+    req.login(user, function(err) {
+        if(err) {
+            console.log(err);
         }
-    }).
-    catch(error => res.json({error: error.message}));
+        else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
+        }
+    });
+
 });
 
-app.get("/register", (req, res) => {
+app.route("/logout")
+
+.get((req, res) => {
+    req.logout(err => {
+        if(err) {
+            console.log(err);
+        }
+    });
+    res.redirect("/");
+});
+
+/*
+    Authorization using bycrypt
+*/
+
+// app.post("/login", (req, res) => {
+//     const username = req.body.username;
+//     const password = req.body.password;
+
+//     User.findOne({email: username}).
+//     then(userFound => {
+//         if(userFound) {
+//             // Load hash from your password DB.
+//             bcrypt.compare(password, userFound.password, function(err, result) {
+//                 if(result === true) {
+//                     res.render("secrets");
+//                 }
+//             });
+//         }
+//     }).
+//     catch(error => res.json({error: error.message}));
+// });
+
+app.get("/secrets", (req, res) => {
+    if(req.isAuthenticated()) {
+        res.render("secrets");
+    }
+    else {
+        res.redirect("/login");
+    }
+});
+
+
+app.route("/register")
+
+.get( (req, res) => {
     res.render("register");
 })
 
+.post((req, res) => {
+    
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if(err) {
+            console.log(err);
+            res.redirect("/register");
+        }
+        else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            })
+        }
+    })
+
+});
+
+/*
+    Registering users using bycrypt
+
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save().
+        then(result => {
+            res.render("secrets");
+        }).
+        catch(error => res.json({error: error.message}));
     });
-    newUser.save().
-    then(result => {
-        res.render("secrets");
-    }).
-    catch(error => res.json({error: error.message}));
+
 })
+
+*/
 
 
 app.listen(3000, () => {
